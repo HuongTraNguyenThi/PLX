3836c8eb03184ce96a140af53398ed638f06fb80
Binh Le <levanbinh1605@gmail.com>
2021-07-07 21:45:46 +0700

    Log exception

--- PLX.API/MiddleWare/ErrorHandlingMiddleware.cs
+++ PLX.API/MiddleWare/ErrorHandlingMiddleware.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using PLX.API.Constants;
 using PLX.API.Data.DTO;
 using PLX.API.Extensions;
@@ -12,10 +13,12 @@ namespace PLX.API.MiddleWare
     public class ErrorHandlerMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlerMiddleware> _logger;
 
-        public ErrorHandlerMiddleware(RequestDelegate next)
+        public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task Invoke(HttpContext context)
@@ -36,6 +39,7 @@ namespace PLX.API.MiddleWare
                         response.StatusCode = (int)HttpStatusCode.InternalServerError;
                         break;
                 }
+                _logger.LogError(error, "--- Fail to process {0}", context.Request.Path);
                 var errorResponse = new ApiErrorResponse(ResultCodeConstants.EInternalServerError, null);
                 await response.WriteAsync(errorResponse.ToJson());
             }
