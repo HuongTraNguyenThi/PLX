5a5d95395b65bee30af61f64db790ac6d1b07115
Nguyen Thi Huong Tra <huongtra1199@gmail.com>
2021-07-08 23:07:18 +0700

    modify GetCustomerById

--- PLX.API/Data/DTO/Customer/GetCustomerResponse.cs
+++ PLX.API/Data/DTO/Customer/GetCustomerResponse.cs
@@ -6,7 +6,7 @@ namespace PLX.API.Data.DTO.Customer
 {
     public class GetCustomerResponse : ResultMessageResponse
     {
-        public CustomerUpdates Customer { get; set; }
+        public CustomerInfoResponse Customer { get; set; }
         public ICollection<VehicleResponse> Vehicles { get; set; }
         public ICollection<LinkedCardResponse> LinkedCards { get; set; }
         public ICollection<QuestionResponse> Questions { get; set; }
