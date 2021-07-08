d928a7f5e2648e618ecc73a7d35c41036b3a6c06
Binh Le <levanbinh1605@gmail.com>
2021-07-08 11:46:38 +0700

    Update phone max length

---
+++ PLX.Persistence.EF/Migrations/20210708044344_UpdatePhoneMaxLength.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PLX.Persistence.EF.Migrations
+{
+    public partial class UpdatePhoneMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customer",
+                type: "character varying(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customer",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(12)",
+                oldMaxLength: 12);
+        }
+    }
+}
